edf9a5ab775a7dd8e6b1a1adf2c832dd1b84c582
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-06-24 20:20:30 +1000

    Implement login form submission handling in LoginPage component; integrate Ant Design for user feedback and utilize Next.js router for navigation.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,22 @@
 // booksys/src/app/login/page.tsx
 "use client";
 import React from "react";
-
+import { useRouter } from "next/navigation";
+import { message } from "antd";
+import request from "@/app/utils/request";
 export default function LoginPage() {
+  const router = useRouter();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(res);
+  };
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-96">
         <h1 className="text-2xl font-bold mb-6">登录</h1>
         {/* 这里可以放表单 */}
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block mb-1">用户名</label>
             <input className="w-full border rounded px-3 py-2" />
